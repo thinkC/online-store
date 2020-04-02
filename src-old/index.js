7a74087413a944c0a3546103a39c036188eddb23
Tunde Oyewo <tunde.oyewo@gmail.com>
2020-04-02 10:01:20 +0100

    first commit

--- src-old/index.js
+++ src-old/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ProductProvider } from './context';
 import * as serviceWorker from './serviceWorker';
 
-
 ReactDOM.render(
-  <ProductProvider>
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>
-  </ProductProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
